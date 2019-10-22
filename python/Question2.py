4e9bca7643faf73b8f7cf615e52df8183e4fe750
kalash013 <45064820+kalash013@users.noreply.github.com>
2019-10-22 23:53:47 +0530

    sd

--- python/Question2.py
+++ python/Question2.py
@@ -1,5 +1,19 @@
 # modify this function, and create other functions below as you wish
 def question02(risk, bonus, trader):
     # modify and then return the variable below
-    answer = -5
+    lis=[]
+    n=len(bonus)
+    for i in range(n):
+        lis.append([risk[i],bonus[i]])
+    lis.sort()
+    trader.sort()
+    r=-1
+    answer = 0
+    mx=0
+    for l in range(n):
+        while(r+1<n and lis[r+1][0]<=trader[l]):
+            r+=1
+            mx=max(mx,lis[r][1])
+        answer+=mx
     return answer
+
